a8dce0dd9d482e56c47168d8446110687145c31f
Mrowr Purr <mrowr.purr@gmail.com>
2021-09-07 08:54:26 -0700

    Initial commit with initial !roll prototype

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MrowrPurr {
+    class Program {
+        static int enterPressCount = 0;
+        static void Main(string[] args) {
+            var bot = new MrowrBot();
+            Console.WriteLine("Running MrowrBot");
+            Console.WriteLine("[PRESS <ENTER> TWICE TO EXIT]");
+            bot.Connect();
+            while (enterPressCount < 2) {
+                var key = Console.ReadKey();
+                if (key.Key == ConsoleKey.Enter)
+                    enterPressCount++;
+                else
+                    enterPressCount = 0;
+            }
+            Console.WriteLine("Existing MrowrBot...");
+            bot.Disconnect();
+        }
+    }
+}
+
